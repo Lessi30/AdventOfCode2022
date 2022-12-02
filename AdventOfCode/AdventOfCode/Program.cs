9999e347a998834efe1dd7e1f45459eadd718892
xAlessio <alessio.vangelisti04@gmail.com>
2022-12-02 08:56:14 +0100

    Day 1 & 2

---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,45 @@
+﻿
+void Day1_Task1()
+{
+    string[] lines = File.ReadAllLines("../../../input.txt");
+    List<int> elves = new List<int>();
+    int calories = 0;
+
+    foreach (string line in lines)
+    {
+        if (line == "")
+        {
+            elves.Add(calories);
+            calories = 0;
+            continue;
+        }
+        calories += Int32.Parse(line);
+    }
+
+    elves.Sort();
+    Console.WriteLine($"Day 1: {elves[elves.Count - 1]}");
+}
+
+void Day1_Task2()
+{
+    string[] lines = File.ReadAllLines("../../../input.txt");
+    List<int> elves = new List<int>();
+    int calories = 0;
+
+    foreach (string line in lines)
+    {
+        if (line == "")
+        {
+            elves.Add(calories);
+            calories = 0;
+            continue;
+        }
+        calories += Int32.Parse(line);
+    }
+
+    elves.Sort();
+    Console.WriteLine($"Day 2: {elves[elves.Count - 1] + elves[elves.Count - 2] + elves[elves.Count - 3]}");
+}
+
+Day1_Task1();
+Day1_Task2();
---
+++ AdventOfCode/AdventOfCode_2/Program.cs
@@ -0,0 +1,118 @@
+﻿void Day2_Task1()
+{
+    int score = 0;
+    string[] lines = File.ReadAllLines("../../../input.txt");
+    foreach (string line in lines)
+    {
+        string[] chooses = line.Split(" ");
+        switch (chooses[0])
+        {
+            case "A":
+                switch (chooses[1])
+                {
+                    case "X":
+                        score += 4;
+                        continue;
+                    case "Y":
+                        score += 8;
+                        continue;
+                    case "Z":
+                        score += 3;
+                        continue;
+                }
+                break;
+
+            case "B":
+                switch (chooses[1])
+                {
+                    case "X":
+                        score += 1;
+                        continue;
+                    case "Y":
+                        score += 5;
+                        continue;
+                    case "Z":
+                        score += 9;
+                        continue;
+                }
+                break;
+
+            case "C":
+                switch (chooses[1])
+                {
+                    case "X":
+                        score += 7;
+                        continue;
+                    case "Y":
+                        score += 2;
+                        continue;
+                    case "Z":
+                        score += 6;
+                        continue;
+                }
+                break;
+        }
+    }
+    Console.WriteLine($"Your score is {score}");
+}
+
+void Day2_Task2()
+{
+    int score = 0;
+    string[] lines = File.ReadAllLines("../../../input.txt");
+    foreach (string line in lines)
+    {
+        string[] chooses = line.Split(" ");
+        switch (chooses[0])
+        {
+            case "A":
+                switch (chooses[1])
+                {
+                    case "X":
+                        score += 3;
+                        continue;
+                    case "Y":
+                        score += 4;
+                        continue;
+                    case "Z":
+                        score += 8;
+                        continue;
+                }
+                break;
+
+            case "B":
+                switch (chooses[1])
+                {
+                    case "X":
+                        score += 1;
+                        continue;
+                    case "Y":
+                        score += 5;
+                        continue;
+                    case "Z":
+                        score += 9;
+                        continue;
+                }
+                break;
+
+            case "C":
+                switch (chooses[1])
+                {
+                    case "X":
+                        score += 2;
+                        continue;
+                    case "Y":
+                        score += 6;
+                        continue;
+                    case "Z":
+                        score += 7;
+                        continue;
+                }
+                break;
+        }
+    }
+    Console.WriteLine($"Your score is {score}");
+}
+
+Day2_Task1();
+Day2_Task2();
