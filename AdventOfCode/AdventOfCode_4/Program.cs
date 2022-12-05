921c1a428ca97d4bca01f1390dd999b0886dfe52
xAlessio <alessio.vangelisti04@gmail.com>
2022-12-05 10:17:43 +0100

    Day 4 Task 2

--- AdventOfCode/AdventOfCode_4/Program.cs
+++ AdventOfCode/AdventOfCode_4/Program.cs
@@ -24,4 +24,33 @@
     Console.WriteLine($"Total pairs: {totalAssignmentPairs}");
 }
 
+void Day4_Task2()
+{
+    int totalOverlappingPairs = 0;
+
+    string[] lines = File.ReadAllLines("../../../input.txt");
+    foreach (string line in lines)
+    {
+        string[] sections = line.Split(",");
+        string[] range1 = sections[0].Split("-");
+        string[] range2 = sections[1].Split("-");
+
+        int range1Start = Int32.Parse(range1[0]);
+        int range1End = Int32.Parse(range1[1]);
+        int range2Start = Int32.Parse(range2[0]);
+        int range2End = Int32.Parse(range2[1]);
+
+        if (range2Start >= range1Start && range2Start <= range1End ||
+            range2End >= range1Start && range2End <= range1End ||
+            range1Start >= range2Start && range1Start <= range2End ||
+            range1End >= range2Start && range1End <= range2End)
+        {
+            totalOverlappingPairs++;
+        }
+    }
+
+    Console.WriteLine($"Total overlapping pairs: {totalOverlappingPairs}");
+}
+
 Day4_Task1();
+Day4_Task2();
