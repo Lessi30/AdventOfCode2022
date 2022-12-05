2bf4963c1b32e0e443b4871b25f4cd5f4129d9e6
xAlessio <alessio.vangelisti04@gmail.com>
2022-12-05 08:58:23 +0100

    Day 3 Task 1

---
+++ AdventOfCode/AdventOfCode_3/Program.cs
@@ -0,0 +1,37 @@
+﻿void Day3_Task1()
+{
+    int totalPriority = 0;
+    List<char> doubledCharacters = new List<char>();
+
+    string[] lines = File.ReadAllLines("../../../input.txt");
+    foreach (string line in lines)
+    {
+        string firstHalf = line.Substring(0, line.Length / 2);
+        char[] characters = firstHalf.ToCharArray();
+        char[] characters2 = line.Substring(line.Length / 2).ToCharArray();
+        foreach (char character in characters)
+        {
+            if (characters2.Contains(character))
+            {
+                doubledCharacters.Add(character);
+                break;
+            }
+        }
+    }
+
+    foreach (char doubledCharacter in doubledCharacters)
+    {
+        if (doubledCharacter > 64 && doubledCharacter < 91)
+        {
+            totalPriority += (doubledCharacter - 38);
+        }
+        else
+        {
+            totalPriority += (doubledCharacter - 96);
+        }
+    }
+
+    Console.WriteLine($"Total priority is {totalPriority}");
+}
+
+Day3_Task1();
